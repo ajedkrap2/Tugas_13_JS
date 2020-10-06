9aa6f5387df7827b90b92a8fa994a27e74db6ebb
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:32:46 +0700

    submit

---
+++ tugas13.js
@@ -0,0 +1,8 @@
+function tugas13() {
+  var namaBuahbuahan = ['Pisang','Jeruk'];
+  console.log(namaBuahbuahan);
+  namaBuahbuahan.unshift('Apel','Mangga');
+  return namaBuahbuahan;
+}
+
+console.log(tugas13());
